443ab12997ed7dd54e089e0855c6678ba5f6d046
Paula <>
2019-09-16 19:11:02 +0200

    modificacion profile para localizacion

--- servidor/models/localizaciones.js
+++ servidor/models/localizaciones.js
@@ -18,21 +18,24 @@ const getByIdLoc = (pId) => {
     })
 }
 
-const updateLoc = (pId, { direccion, latitud, longitud, cp }) => {
+const updateLoc = (pId, { latitud, longitud }) => {
     return new Promise((resolve, reject) => {
-        db.get().query('update localizaciones set direccion = ?, latitud = ?, longitud = ?, cp = ? where id = ?', [direccion, latitud, longitud, cp], (err, result) => {
+        db.get().query('update localizaciones set latitud = ?, longitud = ? where id = ?', [latitud, longitud], (err, result) => {
             if (err) reject(err);
             resolve(result);
         });
     })
 }
 
-const insert = ({ nombre, apellidos, fecha_nacimiento, email, usuario, password }) => {
+const insertLoc = ({ latitud, longitud }) => {
+
     return new Promise((resolve, reject) => {
-        let q = 'insert into usuarios (nombre, apellidos, fecha_nacimiento, email, usuario, password) values (?, ?, ?, ?, ?, ?)';
-        db.get().query(q, [nombre, apellidos, fecha_nacimiento, email, usuario, password], (err, result) => {
+        console.log(latitud, longitud)
+        let q = 'insert into localizaciones (latitud, longitud) values (?, ?)';
+        db.get().query(q, [latitud, longitud], (err, result) => {
             if (err) reject(err)
             resolve(result)
+            console.log(result)
         });
     })
 }
@@ -41,6 +44,7 @@ module.exports = {
 
     getAllLoc: getAllLoc,
     getByIdLoc: getByIdLoc,
-    updateLoc: updateLoc
+    //updateLoc: updateLoc,
+    insertLoc: insertLoc
 
 }
--- servidor/models/usuarios.js
+++ servidor/models/usuarios.js
@@ -4,9 +4,9 @@ const db = require('../db');
 
 const getById = (pId) => {
     return new Promise((resolve, reject) => {
-        db.get().query('select * from usuarios where id = ?', [pId], (err, rows) => {
+        db.get().query('select usuarios.* ,localizaciones.`latitud`, localizaciones.`longitud` from usuarios, localizaciones where usuarios.id = ? AND (usuarios.fk_partida = localizaciones.id OR usuarios.fk_destino = localizaciones.id )', [pId], (err, rows) => {
             if (err) reject(err)
-            rows.length == 1 ? resolve(rows[0]) : resolve(null)
+            rows.length == 2 ? resolve(rows[0], rows[1]) : resolve(null)
         })
     })
 }
@@ -16,13 +16,24 @@ const getByUsername = (pUsername) => {
         db.get().query('select * from usuarios where usuario = ?', [pUsername], (err, rows) => {
             if (err) reject(err)
             if (rows.length != 1) resolve(null)
+
             resolve(rows[0])
         })
     })
 }
-const update = (pId, { nombre, apellidos, fecha_nacimiento, email, usuario, password }) => {
+const update = (pId, { nombre, apellidos, fecha_nacimiento, email, usuario }) => {
     return new Promise((resolve, reject) => {
         db.get().query('update usuarios set nombre = ?, apellidos = ?, fecha_nacimiento = ?, email = ?, usuario = ? where id = ?', [nombre, apellidos, fecha_nacimiento, email, usuario, pId], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    })
+}
+
+const updateLoc = (pId, { fk_partida, fk_destino }) => {
+    console.log(pId, fk_partida, fk_destino);
+    return new Promise((resolve, reject) => {
+        db.get().query('update usuarios set fk_partida = ?, fk_destino = ? where id = ?', [fk_partida, fk_destino, pId], (err, result) => {
             if (err) reject(err);
             resolve(result);
         });
@@ -33,6 +44,7 @@ module.exports = {
     // insert: insert,
     getById: getById,
     getByUsername: getByUsername,
-    update: update
+    update: update,
+    updateLoc: updateLoc
 
 }
